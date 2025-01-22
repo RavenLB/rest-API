c882fe4018b0e98a5b1587fff4708c57c979f8fd
RavenLB <sandra.poturalska@gmail.com>
2025-01-22 11:17:14 +0100

    weather API with flask - basic functions

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")#the values that users add will be stored in the station and date arguments
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
