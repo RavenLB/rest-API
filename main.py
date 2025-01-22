1e4aa923c4c7a149bc835c68d5014b350aff195a
RavenLB <sandra.poturalska@gmail.com>
2025-01-22 11:38:13 +0100

    specifying a port

--- main.py
+++ main.py
@@ -15,3 +15,4 @@ def about(station, date):
 
 if __name__ == "__main__":
     app.run(debug=True)
+   # app.run(debug=True, port=5001)if you have multiple apps running at once you can specify the port here
